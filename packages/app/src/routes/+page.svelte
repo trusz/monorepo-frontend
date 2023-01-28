f1426a79e083cf77414da87c277651b7d7ef4b59
Tamás Rusz <t.rusz@sprinteins.com>
2023-01-28 17:25:27 +0100

    feat: add turbo repo

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -3,9 +3,11 @@
 	import { Button } from "@someproject/uilib"
 	const john = new Greeter()
 	john.sayHello()
+
+
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Button label="click me" />
+<Button label="click me"/>
